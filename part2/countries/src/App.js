200004c8b97892c23919dd64d81560fd1cece2fd
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-05-17 14:49:02 +0300

    Part 2 exercises

---
+++ part2/countries/src/App.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+// TODO 2.13-2.14
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [countryFilter, setCountryFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(response => setCountries(response.data))
+  }, [])
+
+  const c = countries.filter(country => country.name.common.toLowerCase().includes(countryFilter.toLowerCase()))
+  return (
+    <>
+      <div>
+        <p>Find country: <input value={countryFilter} onChange={(event) => setCountryFilter(event.target.value)}/></p>
+      </div>
+      <div>
+        {c.length > 10
+          ? <p>Too many countries</p>
+          : c.length > 1
+            ? c.map(country => <p key={country.cca2}>{country.name.common}</p>)
+            : c.map(country => {
+              return <div key={country.cca2}>
+                <h2>{country.name.common}</h2>
+                <p>Capital: {country.capital}</p>
+                <p>Population: {country.population}</p>
+                <h3>Languages:</h3>
+                <ul>
+                  {Object.values(country.languages).map((lang) => <li key={lang}>{lang}</li>)}
+                </ul>
+                <img src={country.flags.svg} alt=''/>
+              </div>
+            })
+        }
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react'
+import PhonebookService from './services/PhonebookService'
+import Phonebook from './components/Phonebook'
+
+//TODO 2.18, 2.20
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
+  const [dbMessage, setDbMessage] = useState('')
+  useEffect(() => {
+    PhonebookService
+      .getAllPersons()
+      .then(data => {
+        setPersons(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Phonebook values={{persons, newName, newPhone, filter, dbMessage}}
+                 handlers={{setPersons, setNewName, setNewPhone, setFilter, setDbMessage}} service={PhonebookService}/>
+    </div>
+  )
+}
+
+export default App
